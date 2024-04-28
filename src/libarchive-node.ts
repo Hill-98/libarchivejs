40fb2f99a529f627a0a3b1efca94a2f7dd5d2f92
Zhong Lufan (钟路帆) <lufanzhong@gmail.com>
2024-04-29 02:22:24 +0800

    Fix NodeJS not loading worker on Windows

--- src/libarchive-node.ts
+++ src/libarchive-node.ts
@@ -1,11 +1,12 @@
 import { Worker } from "worker_threads";
-import { URL } from "url";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 import * as Comlink from "comlink";
 import nodeEndpoint from "comlink/dist/esm/node-adapter";
 import { Archive } from "./libarchive";
 export * from "./libarchive";
 
-const __dirname = new URL(".", import.meta.url).pathname;
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 Archive.init({
   getWorker: () => new Worker(`${__dirname}/worker-bundle-node.mjs`),
